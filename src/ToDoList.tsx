c9794acb50d79b01288d7f9392289364080022fe
JE <wjd7dms7@naver.com>
2023-07-14 03:16:23 +0900

    #6.9 Custom Validation

--- src/ToDoList.tsx
+++ src/ToDoList.tsx
@@ -1,6 +1,5 @@
 // import React, { useState } from "react";
 import { useForm } from "react-hook-form";
-import { DefaultValue } from "recoil";
 
 /* function ToDoList() {
   const [toDo, setToDo] = useState("");
@@ -31,25 +30,42 @@ import { DefaultValue } from "recoil";
 } */
 
 interface IForm {
-    email: string;
-    firstName: string;
-    lastName: string;
-    username: string;
-    password: string;
-    password1: string;
+  email: string;
+  firstName: string;
+  lastName: string;
+  username: string;
+  password: string;
+  password1: string;
+  extraError?: string;
 }
 
 function ToDoList() {
-  const { register, watch, handleSubmit, formState:{errors} } = useForm<IForm>({
+  const {
+    register,
+    watch,
+    handleSubmit,
+    formState: { errors },
+    setError,
+  } = useForm<IForm>({
     defaultValues: {
-        email: "@naver.com"
-    }
+      email: "@naver.com",
+    },
   });
   // register : 어떤 input을 관리하는지 (key)
   // watch : onChange 대체
   // handleSubmit : submit 대체
-  const onValid = (data: any) => {
-    console.log(data);
+  const onValid = (data: IForm) => {
+    if (data.password !== data.password1) {
+      setError(
+        "password1",
+        { message: "패스워드가 일치하지 않습니다" },
+        { shouldFocus: true } // add버튼을 클릭했을때 error메세지가 존재하는 input에 자동 포커스
+      );
+    }
+    // setError("extraError", {message: "sever offline"})
+    // ㄴ 특정항목에 해당되는 에러가 아닌, 전체 form에 해당되는 에러
+    // ㄴㄴ(추가적인 에러가 필요하다면 항목의 이름을 새로 지어주고 사용하면 된다)
+    // console.log(data);
   };
   //   console.log(watch());
   console.log(errors);
@@ -63,16 +79,29 @@ function ToDoList() {
           {...register("email", {
             required: "이메일은 필수 입력 항목입니다",
             pattern: {
-                value: /^[A-Za-z0-9._%+-]+@naver.com$/,
-                message: "naver.com 주소만 허용됩니다.",
-            }
+              value: /^[A-Za-z0-9._%+-]+@naver.com$/,
+              message: "naver.com 주소만 허용됩니다.",
+            },
           })}
           placeholder="Email"
         />
         {/* <span>{errors?.email?.message}</span> */}
         <span>{errors?.email?.message as string}</span>
         <input
-          {...register("firstName", { required: "필수 입력 항목입니다" })}
+          {...register("firstName", {
+            required: "필수 입력 항목입니다",
+            // validate: (value) => !value.includes("nico"), // value에 nico가 포함되지 않는다면 true반환
+            // validate: (value) =>
+            //   value.includes("nico")
+            //     ? "nico를 포함한 값은 허용되지 않습니다"
+            //     : true,
+            validate: {
+                noNico: (value) =>
+                    value.includes("nico") ? "nico를 포함한 값은 허용되지 않습니다" : true,
+                noNick: (value) =>
+                    value.includes("nick") ? "nick를 포함한 값은 허용되지 않습니다" : true,
+            },
+          })}
           placeholder="First Name"
         />
         <span>{errors?.firstName?.message as string}</span>
@@ -82,7 +111,10 @@ function ToDoList() {
         />
         <span>{errors?.lastName?.message as string}</span>
         <input
-          {...register("username", { required: "필수 입력 항목입니다", minLength: 10 })}
+          {...register("username", {
+            required: "필수 입력 항목입니다",
+            minLength: 10,
+          })}
           placeholder="Username"
         />
         <span>{errors?.username?.message as string}</span>
@@ -98,11 +130,15 @@ function ToDoList() {
         />
         <span>{errors?.password?.message as string}</span>
         <input
-          {...register("password1", { required: "필수 입력 항목입니다", minLength: 5 })}
+          {...register("password1", {
+            required: "필수 입력 항목입니다",
+            minLength: 5,
+          })}
           placeholder="Password1"
         />
         <span>{errors?.password1?.message as string}</span>
         <button>Add</button>
+        <span>{errors?.extraError?.message as string}</span>
       </form>
     </div>
   );
