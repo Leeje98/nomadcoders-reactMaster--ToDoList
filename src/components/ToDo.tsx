0bf84e871a077e6702dcd58aa6e5f0ebb208f92f
JE <wjd7dms7@naver.com>
2023-07-14 15:57:05 +0900

    #6.13 Categories

--- src/components/ToDo.tsx
+++ src/components/ToDo.tsx
@@ -1,15 +1,26 @@
-import React from 'react'
-import { IToDo } from '../atoms'
+import React from "react";
+import { IToDo, toDoState } from "../atoms";
+import { useSetRecoilState } from "recoil";
 
-function ToDo({text}:IToDo) {
+function ToDo({ text, category, id }: IToDo) {
+    const setToDos = useSetRecoilState(toDoState);
+  const onClick = (newCategory: IToDo["category"]) => {
+    console.log(newCategory);
+  };
   return (
     <li>
-        <span>{text}</span>
-        <button>To Do</button>
-        <button>Doing</button>
-        <button>Done</button>
+      <span>{text}</span>
+      {category !== "DOING" && (
+        <button onClick={() => onClick("DOING")}>Doing</button>
+      )}
+      {category !== "TO_DO" && (
+        <button onClick={() => onClick("TO_DO")}>To Do</button>
+      )}
+      {category !== "DONE" && (
+        <button onClick={() => onClick("DONE")}>Done</button>
+      )}
     </li>
-  )
+  );
 }
 
-export default ToDo
+export default ToDo;
