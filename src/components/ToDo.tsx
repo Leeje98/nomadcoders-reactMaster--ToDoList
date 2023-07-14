1673e0350eb5fc35fc02a2760b0784e617aa4e28
JE <wjd7dms7@naver.com>
2023-07-15 03:01:17 +0900

    #6.15 Immutability part Two

--- src/components/ToDo.tsx
+++ src/components/ToDo.tsx
@@ -15,10 +15,14 @@ function ToDo({ text, category, id }: IToDo) {
         const targetIndex = oldToDos.findIndex(toDo => toDo.id === id)
                                    // findIndex() : 주어진 함수를 통과한 첫 번째 요소의 인덱스 값 반환
                                                  // 조건에 맞는 요소를 찾을 수 없다면 -1을 반환
-        const oldToDo = oldToDos[targetIndex];
-        const newToDo = {text, id, category: name};
-        console.log(oldToDo, newToDo);
-        return oldToDos
+        // const oldToDo = oldToDos[targetIndex];
+        const newToDo = {text, id, category: name as any}; // as any : 타입스크립트의 문법을 탈출
+        // console.log(oldToDo, newToDo);
+        return [
+            ...oldToDos.slice(0, targetIndex),
+            newToDo,
+            ...oldToDos.slice(targetIndex + 1),
+        ]
     })
   };
   return (
@@ -26,15 +30,15 @@ function ToDo({ text, category, id }: IToDo) {
       <span>{text}</span>
       {category !== "DOING" && (
         // <button onClick={() => onClick("DOING")}>Doing</button>
-        <button name="Doing" onClick={onClick}>Doing</button>
+        <button name="DOING" onClick={onClick}>Doing</button>
       )}
       {category !== "TO_DO" && (
         // <button onClick={() => onClick("TO_DO")}>To Do</button>
-        <button name="to_Do" onClick={onClick}>To Do</button>
+        <button name="TO_DO" onClick={onClick}>To Do</button>
       )}
       {category !== "DONE" && (
         // <button onClick={() => onClick("DONE")}>Done</button>
-        <button name="Done" onClick={onClick}>Done</button>
+        <button name="DONE" onClick={onClick}>Done</button>
       )}
     </li>
   );
