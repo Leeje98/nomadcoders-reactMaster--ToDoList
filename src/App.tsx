b632e651ea156cedb20d25463956e5f0afc29ab2
JE <wjd7dms7@naver.com>
2023-07-22 02:51:18 +0900

    #7.0 Get Selectors

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,23 @@
- 
+import { useRecoilState, useRecoilValue } from "recoil";
+import { hourSelector, minuteState } from "./atoms";
 
 function App() {
-  return ( 
-    <div></div>
+  const [minutes, setMinutes] = useRecoilState(minuteState);
+  const hours = useRecoilValue(hourSelector)
+  const onMinutesChange = (event: React.FormEvent<HTMLInputElement>) => {
+    setMinutes(+event.currentTarget.value);
+    // + 를 붙여주게 되면 string 값을 number값으로 변환함
+  };
+  return (
+    <div>
+      <input
+        value={minutes}
+        onChange={onMinutesChange}
+        type="number"
+        placeholder="Minutes"
+      />
+      <input value={hours} type="number" placeholder="Hours" />
+    </div>
   );
 }
 
