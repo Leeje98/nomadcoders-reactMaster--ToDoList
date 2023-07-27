441af7fffd1b3646de74335a4d2ee0e8c05b112d
JE <wjd7dms7@naver.com>
2023-07-28 00:35:13 +0900

    #7.1 Set Selectors

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
-import { useRecoilState, useRecoilValue } from "recoil";
+import { useRecoilState } from "recoil";
 import { hourSelector, minuteState } from "./atoms";
 
 function App() {
   const [minutes, setMinutes] = useRecoilState(minuteState);
-  const hours = useRecoilValue(hourSelector)
+  const [hours, setHours] = useRecoilState(hourSelector);
+  // 첫번째는 selector의 get, 두번재는 set
   const onMinutesChange = (event: React.FormEvent<HTMLInputElement>) => {
     setMinutes(+event.currentTarget.value);
     // + 를 붙여주게 되면 string 값을 number값으로 변환함
+  };
+  const onHoursChange = (event: React.FormEvent<HTMLInputElement>) => {
+    setHours(+event.currentTarget.value);
   };
   return (
     <div>
@@ -16,7 +20,12 @@ function App() {
         type="number"
         placeholder="Minutes"
       />
-      <input value={hours} type="number" placeholder="Hours" />
+      <input
+        value={hours}
+        onChange={onHoursChange}
+        type="number"
+        placeholder="Hours"
+      />
     </div>
   );
 }
--- src/atoms.tsx
+++ src/atoms.tsx
@@ -1,14 +1,20 @@
+import { set } from "react-hook-form";
 import { atom, selector } from "recoil"; 
 
 export const minuteState = atom({
-  key: "minute",
+  key: "minutes",
   default: 0,
 })
 
-export const hourSelector = selector({
+export const hourSelector = selector<number>({
   key: "hours",
   get: ({ get }) => {
     const minutes = get(minuteState);
     return minutes / 60;
-  }
+  },
+  set: ({ set }, newValue) => {
+    const minutes = Number(newValue) * 60; 
+    set(minuteState, minutes);
+    // 첫번째: 수정하고 싶은 atom / 두번째: 넣어줄 값
+  },
 })
